4ff9af1dd2d87d70ce7a56484b0886cb9afef36c
Pablo Fajardo <pfajardodiaz17@gmail.com>
2021-06-14 17:56:11 -0500

    new

--- EssentialGo/Fundamentals2/mutable_inmutable.go
+++ EssentialGo/Fundamentals2/mutable_inmutable.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := 5
+	y := x
+	x = 10
+
+	//A and B point to same slice in the memory, use the same address
+	var a []int = []int{3, 4, 5}
+	//b is not a copy
+	b := a
+
+	//If you modify a or b, changes the slice
+	b[0] = 100
+	a[1] = 200
+
+	fmt.Println(x, y)
+	fmt.Println(a, b)
 }
